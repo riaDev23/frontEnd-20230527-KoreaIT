b606c924248b2f733e8e39b2efc7b0dfdca842eb
riadev23 <riadev@proton.me>
2023-06-24 15:34:35 +0900

    20230624

--- js/environment.js
+++ js/environment.js
@@ -1,4 +1,15 @@
 let heading = document.querySelector("#heading");
+let confirmBtn = document.querySelector("#confirmBtn");
+let promptBtn = document.querySelector("#promptBtn");
+
 heading.onclick = function () {
   heading.style.color = "red";
+};
+
+confirmBtn.onclick = function () {
+  console.log(confirm("컴펌창입니다."));
+};
+
+promptBtn.onclick = function () {
+  console.log(prompt("입력해주세요"));
 };
--- ria_react/src/App.js
+++ ria_react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          프론트엔드
         </p>
         <a
           className="App-link"
